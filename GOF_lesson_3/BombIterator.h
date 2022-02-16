f692ec6ea360b431a215e31f99f6695d8468f8f3
Alexandr <kajlaleksandr1986@gmail.com>
2022-02-16 12:01:17 +0700

    Добавьте файлы проекта.

---
+++ GOF_lesson_3/BombIterator.h
@@ -0,0 +1,78 @@
+#pragma once
+#include"SBomber.h"
+
+class BombIterator
+{
+	std::vector<DynamicObject*>& refVec;
+	int bombIdx;
+	int idx;
+	bool check;
+
+public:
+	BombIterator(std::vector<DynamicObject*> &_refVec) :refVec(_refVec),bombIdx(0), idx(0),check(false)
+	{
+		for (; idx < refVec.size(); idx++)
+		{
+			Bomb* pBomb = dynamic_cast<Bomb*>(refVec[idx]);
+			if (pBomb != nullptr)
+			{
+				bombIdx = idx++;
+				check = true;
+				return;
+			}
+		}
+	}
+
+
+	Bomb* operator++ () // префиксный инкремент
+	{
+
+		for (; idx < refVec.size(); idx++)
+		{
+			Bomb* pBomb = dynamic_cast<Bomb*>(refVec[idx]);
+			if (pBomb != nullptr)
+			{
+				bombIdx = idx++;
+				check = true;
+				return pBomb;
+
+			}
+		}
+
+		check = false;
+
+		return nullptr;
+	}
+
+	Bomb* operator-- () // префиксный инкремент
+	{
+
+		for (; idx >= 0; idx--)
+		{
+			Bomb* pBomb = dynamic_cast<Bomb*>(refVec[idx]);
+			if (pBomb != nullptr)
+			{
+				bombIdx = idx--;
+				check = true;
+				return pBomb;
+
+			}
+		}
+
+		check = false;
+		return nullptr;
+	}
+
+	Bomb* operator*()
+	{
+		
+		Bomb* pBomb = dynamic_cast<Bomb*>(refVec[bombIdx]);
+		return pBomb;
+	}
+
+	bool checkIt()
+	{
+		return check;
+	}
+
+};
---
+++ GOF_lesson_3/SBomber.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+
+#include "LevelGUI.h"
+#include "Plane.h"
+#include "Bomb.h"
+#include "Ground.h"
+#include "Tank.h"
+
+class SBomber
+{
+public:
+    SBomber();
+    ~SBomber();
+    
+    inline bool GetExitFlag() const { return exitFlag; }
+
+    void ProcessKBHit();
+    void TimeStart();
+    void TimeFinish();
+
+    void DrawFrame();
+    void MoveObjects();
+    void CheckObjects();
+
+private:
+
+    void CheckPlaneAndLevelGUI();
+    void CheckBombsAndGround();
+    void CheckDestoyableObjects(Bomb* pBomb);
+
+    void DeleteDynamicObj(DynamicObject * pBomb);
+    void DeleteStaticObj(GameObject* pObj);
+
+    Ground * FindGround() const;
+    Plane * FindPlane() const;
+    LevelGUI * FindLevelGUI() const;
+    std::vector<DestroyableGroundObject*> FindDestoyableGroundObjects() const;
+    std::vector<Bomb*> FindAllBombs() const;
+
+    void DropBomb();
+	
+	std::vector<GameObject*> vecStaticObj;
+	std::vector<DynamicObject*> vecDynamicObj;
+
+    bool exitFlag;
+
+    uint64_t startTime, finishTime, passedTime;
+    uint16_t bombsNumber, deltaTime, fps;
+    int16_t score;
+};
